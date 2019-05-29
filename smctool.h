431a07aac4a9732b69ec92149dd30c3a7ddd7215
Evgeny Zinoviev <me@ch1p.io>
2019-05-29 16:47:34 +0300

    add missing file

---
+++ smctool.h
@@ -0,0 +1,22 @@
+/*
+ * This file is part of the coreboot project.
+ *
+ * Copyright (C) 2019 Evgeny Zinoviev <me@ch1p.io>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef SMCTOOL_H
+#define SMCTOOL_H
+
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define KEYBUFSIZE 5
+
+#endif /* SMCTOOL_H */
